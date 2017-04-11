a9e952cf657a54dde986c54bd56f326ce7089e38
Faebi <fabian.ruetsche@mmp.htwchur.ch>
2017-04-11 12:50:28 +0200

    keine Ahnung

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>test!</h1>
+    
 
 
 
