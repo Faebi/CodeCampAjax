d1928e8582b707e5b0720f8ff7b4dc1880ef2085
Kim Schläpfer <kimschlaepfer3000@gmail.com>
2017-05-08 17:55:51 +0200

    layout

--- ajax/level1.php
+++ ajax/level1.php
@@ -5,7 +5,7 @@ $genre_list = get_genre();
 
 
   <div id="level_1" class="container">
-    <div class="row">
+    <div class="genretable">
         <?php while ($genre = mysqli_fetch_assoc($genre_list)) { ?>
           <div class="col-6 col-md-2">
           <button type="button" class="btn btn-secondary btn-circle btn-xl" id="genre-<?php echo strtolower($genre['genre']); ?>"><?php echo $genre['genre']; ?></button>
