94d689b7ef156468013e5c2f1a3cc3174c7b1027
Kim Schläpfer <kimschlaepfer3000@gmail.com>
2017-05-08 10:22:12 +0200

    add work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-function ajax(url) {
-return new Promise((resolve, reject) => {
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url);
-xhr.onload = () => { resolve(xhr.responseText); };
-xhr.onerror = () => { reject(xhr.statusCode); };
-xhr.send();
-});
-}
-
-ajax('txt/info.txt')
-.then((data) => {
-// Bei Erfolg!
-})
-.catch((error) => {
-// Bei einem Fehler...
+$("#level-00").click(function(){
+    $.get("", function(data, status){
+        // some action
+    });
 });
