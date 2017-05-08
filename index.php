efb015f48b8d126ba294daad747e75aaa032de5f
Kim Schläpfer <kimschlaepfer3000@gmail.com>
2017-05-08 15:27:24 +0200

    ajax level 1 works :)

--- index.php
+++ index.php
@@ -15,137 +15,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
   </head>
+
+
+
   <body>
+    <h1 class="elegantshd">Gimme musical inspiration</h1>
+
     <div id="ajax">
       <div id="level_0" class="container">
         <div class="col-12">
-          <h1 class="elegantshd">Gimme musical inspiration</h1>
-          <button type="button" class="btn btn-secondary btn-circle btn-xl">Let's Go</button>
+          <button type="button" class="btn btn-secondary btn-circle btn-xl" id="level_0-button">Let's Go</button>
         </div>
       </div>
-
-      <div id="level_1" class="container">
-        <div class="row">
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Indie</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Pop</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Hip-Hop</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Metal</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Future</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Techno</button>
-          </div>
-          <div class="col-6 col-md-2">
-            <button type="button" class="btn btn-secondary btn-circle btn-xl">Hard-Rock</button>
-          </div>
-        </div>
-      </div>
-
-
-      <div id="level_2" class="container">
-        <div  id="songtable" class="row">
-          <div class= "col-12">
-            <table>
-              <tr>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <img src="https://www.topknobs.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/6/M613.jpg" alt="Mountain View" style="width:304px;height:228px;">
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>
-
-
     </div>
 
   <footer class="text-center">
@@ -159,7 +40,7 @@
   </footer>
 
     <!-- jQuery first, then Tether, then Bootstrap JS, then custom JS... -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-window.onload = initPage;
-function initPage() {
 
-$("#level-0 > button").click(function(){
-    $.get("***URL****", function(data, status){
-        // some action
+$('#level_0-button').click(function(){
+  console.log("YES .click works")
+    $.ajax({
+      url: 'ajax/level1.html',
+      dataType: 'html',
+      method: 'GET',
+      data: '',
+      success: function(retdata){
+        $("#ajax").html(retdata);
+      }
+
+    })
+  });
+
+/*
+
+    get('level1.html', function(data, status){
+      $('#ajax').append($( '#level_1' ).html( data ));
+  alert( 'Load was performed.' );
     });
+
+
+$('#level_0-button').click(function(){
+  console.log('succes');
+  $('#ajax').load( 'level1.html' );
 });
-}
+
+$('#level_1 > button').click(function(){
+  $.get('', function(data,status){
+  });
+});
+*/
